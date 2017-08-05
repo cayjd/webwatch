1a917235d5391e5fa6393249baf55f8db09ab18c
Jan Weidner <jw3126@gmail.com>
2017-08-05 09:27:44 +0200

    add webwatch.conf, remove do notation

--- app/Main.hs
+++ app/Main.hs
@@ -9,30 +9,41 @@ import qualified Data.Text as T
 import           System.Environment      (getArgs)
 import           System.Exit             (exitFailure)
 
+import qualified Data.HashSet as HS
+import           Control.Monad.Reader    (ReaderT, ask, runReaderT)
+import           Control.Monad.State     (StateT, evalStateT, state)
+import           Control.Monad.Trans     (liftIO)
+import           System.IO               (hPutStrLn, stderr)
+
 data Config = Config
     { cPatterns        :: [T.Text]
-    , cUrl             :: !T.Text
-    , cInterval        :: !Int
-    , cSlackWebhookUrl :: !T.Text
+    , cUrl             :: T.Text
+    , cInterval        :: Int
+    , cSlackWebhookUrl :: T.Text
     } deriving (Show)
 
 parseConfig :: C.Config -> IO Config
-parseConfig conf = do
-    cPatterns        <- C.require conf "patterns"
-    cUrl             <- C.require conf "url"
-    cInterval        <- C.require conf "interval"
-    cSlackWebhookUrl <- C.require conf "slack.webhook_url"
-    return Config {..}
-
+parseConfig conf = C.require conf "patterns" >>= (\cPatterns -> 
+    C.require conf "url" >>= (\cUrl ->
+    C.require conf "interval" >>= (\cInterval ->
+    C.require conf "slack.webhook_url" >>= (\cSlackWebhookUrl ->
+    return Config {cPatterns=cPatterns, cUrl=cUrl, cInterval=cInterval, cSlackWebhookUrl=cSlackWebhookUrl}))))
 
 main :: IO ()
-main = do
-    args <- getArgs
+main = getArgs >>= (\args ->
     case args of
-        [confPath] -> do
-            config <- parseConfig =<< C.load [C.Required confPath]
-            putStrLn $ "Got a config file:"
-            putStrLn $ show config
-        _ -> do
-            putStrLn "Usage: webwatch CONFIG"
-            exitFailure
+        [confPath] -> showConfigFromPath confPath
+        _ -> putStrLn "Usage: webwatch CONFIG" >>
+            exitFailure)
+
+showConfigFromPath :: String -> IO ()
+showConfigFromPath confPath = C.load [C.Required confPath] >>= (\cConfig ->
+    parseConfig cConfig >>= (\config ->
+    putStrLn "Got a config file:" >>
+    putStrLn (show config)))
+
+type LinkSet = HS.HashSet T.Text
+type WebWatchM = ReaderT Config (StateT LinkSet IO)
+
+slog :: String -> WebWatchM ()
+slog msg = liftIO $ hPutStrLn stderr msg
