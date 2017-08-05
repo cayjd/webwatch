ba660fcb3d9ef4a17aaa579d671439004796fd49
Jan Weidner <jw3126@gmail.com>
2017-08-05 13:37:14 +0200

    put links to command line, filter new links

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,11 @@ import qualified Data.HashSet as HS
 import           Control.Monad.Reader    (ReaderT, ask, runReaderT)
 import           Control.Monad.State     (StateT, evalStateT, state)
 import           Control.Monad.Trans     (liftIO)
+import           Control.Concurrent      (threadDelay)
+import           Control.Monad           (forever, unless)
 import           System.IO               (hPutStrLn, stderr)
+
+import WebWatch.GetLinks
 
 data Config = Config
     { cPatterns        :: [T.Text]
@@ -29,21 +33,43 @@ parseConfig conf = C.require conf "patterns" >>= (\cPatterns ->
     C.require conf "slack.webhook_url" >>= (\cSlackWebhookUrl ->
     return Config {cPatterns=cPatterns, cUrl=cUrl, cInterval=cInterval, cSlackWebhookUrl=cSlackWebhookUrl}))))
 
-main :: IO ()
-main = getArgs >>= (\args ->
-    case args of
-        [confPath] -> showConfigFromPath confPath
-        _ -> putStrLn "Usage: webwatch CONFIG" >>
-            exitFailure)
-
-showConfigFromPath :: String -> IO ()
-showConfigFromPath confPath = C.load [C.Required confPath] >>= (\cConfig ->
-    parseConfig cConfig >>= (\config ->
-    putStrLn "Got a config file:" >>
-    putStrLn (show config)))
-
 type LinkSet = HS.HashSet T.Text
 type WebWatchM = ReaderT Config (StateT LinkSet IO)
 
 slog :: String -> WebWatchM ()
 slog msg = liftIO $ hPutStrLn stderr msg
+
+addLinks :: [Link] -> LinkSet -> ([Link], LinkSet)
+addLinks links set =
+    (new, HS.union set (HS.fromList $ map lHref new))
+  where
+    new = filter (\l -> not $ (lHref l) `HS.member` set) links
+
+watchOnce :: WebWatchM ()
+watchOnce = ask >>= (\config ->
+    (slog $ "Getting links from " ++ T.unpack (cUrl config)) >>
+    (liftIO $ getMatchingLinks (cPatterns config) (cUrl config)) >>= (\links ->
+    (slog $ "All matching links: " ++ show links) >>
+    ((state $ addLinks links) >>= (\newLinks -> slog $ "New Links: " ++ (show newLinks))) >>
+    -- TODO: Send message
+    (slog $ "Sleeping " ++ show (cInterval config) ++ " microsecond(s)") >>
+    (liftIO $ threadDelay (cInterval config))))
+
+main :: IO ()
+main = getArgs >>= (\args ->
+    case args of
+        [confPath] -> C.load [C.Required confPath] >>= 
+            parseConfig >>= 
+            (\c -> (showConfig c >> webWatch c))
+
+        _ -> putStrLn "Usage: webwatch CONFIG" >>
+            exitFailure)
+
+showConfig :: Config -> IO ()
+showConfig config = 
+    putStrLn "Got a config file:" >>
+    putStrLn (show config)
+
+webWatch :: Config -> IO ()
+webWatch config =
+    evalStateT (runReaderT (forever watchOnce) config) HS.empty
---
+++ src/WebWatch/GetLinks.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+module WebWatch.GetLinks
+    ( Link (..)
+    , getMatchingLinks
+    ) where
+    
+import           Data.Maybe              (mapMaybe)
+import qualified Data.Text               as T
+import qualified Data.Text.Lazy          as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import qualified Network.HTTP.Conduit    as Http (parseUrl)
+import qualified Network.HTTP.Simple     as Http
+import qualified Network.URI             as Uri
+import qualified Text.HTML.TagSoup       as TagSoup
+
+data Link = Link
+    { lTitle :: !T.Text
+    , lHref  :: !T.Text
+    } deriving (Show, Eq)
+
+getMatchingLinks
+    :: [T.Text]  -- ^ Patterns to watch out for.
+    -> T.Text    -- ^ The URL of the HTML page we want to check.
+    -> IO [Link] -- ^ All links within that HTML page
+
+getMatchingLinks patterns uri = do
+    req <- Http.parseUrl (T.unpack uri)
+    lbs <- Http.getResponseBody <$> Http.httpLbs req
+
+    -- Convert the 'ByteString' into 'Text'
+    let htmlPageAsText = TL.toStrict $ TL.decodeUtf8 lbs
+
+    return $
+        makeAbsolute uri $
+        matchingLinks patterns $
+        extractLinks htmlPageAsText
+
+extractLinks :: T.Text -> [Link]
+extractLinks = findLinks . TagSoup.parseTags
+  where
+    findLinks (TagSoup.TagOpen "a" args : tags0) =
+        let closeLink = (== TagSoup.TagClose "a") in
+        case (lookup "href" args, break closeLink tags0) of
+            (Nothing,   (_,     tags1))       -> findLinks tags1
+            (_,         (_,     []))          -> []
+            (Just href, (title, (_ : tags1))) ->
+                Link (TagSoup.innerText title) href : findLinks tags1
+
+    findLinks (_ : tags0) = findLinks tags0
+    findLinks [] = []
+
+makeAbsolute :: T.Text -> [Link] -> [Link]
+makeAbsolute url = mapMaybe $ \l -> do
+    base <- baseUri
+    href <- Uri.parseURI $ T.unpack (lHref l)
+    return l {lHref = T.pack $ show $ href `Uri.relativeTo` base}
+  where
+    baseUri = Uri.parseURI $ T.unpack url
+
+matchingLinks :: [T.Text] -> [Link] -> [Link]
+matchingLinks patterns = filter $
+    \l -> any (`T.isInfixOf` T.toLower (lTitle l)) lpatterns
+  where
+    lpatterns = map T.toLower patterns
+
